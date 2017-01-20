a1e43232fb26330d85812f0f7344463835457909
Alina <alinkamog@gmail.com>
2017-01-20 10:18:25 +0200

    Add tooltips on Jquery for contact form
    submit for "request form"

--- js/submit.js
+++ js/submit.js
@@ -1,10 +1,7 @@
-/**
- * Created by иарвар on 12.01.2017.
- */
-function submit() {
-    $( "#form" ).submit(function( event ) {
+
+    $( ".form" ).submit(function( event ) {
         alert( "Handler for .submit() called." );
-       // event.preventDefault();
+        event.preventDefault();
     });
 
-}
+
--- js/ui.js
+++ js/ui.js
@@ -8,4 +8,23 @@
     });
   } );
 
- 
+//ADD TOOLTIPS FOR fORM "Contact me"
+  $( function() {
+    var tooltips = $( "[title]" ).tooltip({
+      position: {
+        my: "left top",
+        at: "right+5 top-5",
+        collision: "none"
+      }
+    });
+    $( "<submit>" )
+      .text( "Show help" )
+      .button()
+      .on( "click", function() {
+        tooltips.tooltip( "open" );
+      })
+      .insertAfter( "form" );
+  } );
+  </script>
+
+
